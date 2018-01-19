5e5f059a55d393d3bceee9a04b2bf91bc6a485fb
Rob Moore <me@robdmoore.id.au>
2018-01-19 10:31:06 +0800

    Updated approach to unzipping node_modules to see if it fixed speed improvements - it didn;t
    2018-01-19 02:31:48Z Installing next.js server site extension
    2018-01-19 02:31:48Z Unzipping node_modules cache
    2018-01-19 02:33:59Z Copying files from D:\home\SiteExtensions\nextjs-server\files to D:\home\site\wwwroot
    2018-01-19 02:39:14Z Finished installing next.js server site extension
    
    2 mins to unzip, 5 mins to copy to wwwroot - ugh!

--- Next.js/install.ps1
+++ Next.js/install.ps1
@@ -1,10 +1,35 @@
-Write-Output "Installing next.js server site extension"
+$ErrorActionPreference = "Stop"
+
+Add-Type -AssemblyName System.IO.Compression.FileSystem
+function Invoke-Unzip
+{
+    param([string]$zipfile, [string]$outpath)
+
+    [System.IO.Compression.ZipFile]::ExtractToDirectory($zipfile, $outpath)
+}
+
+Write-Output "$(Get-Date -Format u) Installing next.js server site extension"
+
 $wwwRoot = $env:WEBROOT_PATH
 $deploymentFiles = Join-Path $PSScriptRoot files
 
-Write-Output "Copying files from $deploymentFiles to $wwwRoot"
-ls $deploymentFiles | Copy-Item -Destination $wwwRoot -Force
+if (Test-Path (Join-Path $deploymentFiles "node_modules")) {
+    Write-Output "$(Get-Date -Format u) Removing existing unzipped node_modules"
+    try {
+        cmd /c "rd /s /q $(Join-Path $deploymentFiles "node_modules")"
+    } catch {
+        Write-Output "WARNING: Error removing files: $_"
+    }
+}
 
-Write-Output "Running npm install in $wwwRoot"
-cd $wwwRoot
-npm install
+Write-Output "$(Get-Date -Format u) Unzipping node_modules cache"
+Invoke-Unzip (Join-Path $PSScriptRoot "node_modules.zip") $deploymentFiles
+
+Write-Output "$(Get-Date -Format u) Copying files from $deploymentFiles to $wwwRoot"
+ls $deploymentFiles | Copy-Item -Destination $wwwRoot -Force -Recurse
+
+#Write-Output "Running npm install in $wwwRoot"
+#cd $wwwRoot
+#npm install
+
+Write-Output "$(Get-Date -Format u) Finished installing next.js server site extension"
